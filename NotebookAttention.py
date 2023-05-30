18833d51f2a46f2fcf2c9a2862a065f38561e229
Pablo Arjonilla <pablomarjonilla@gmail.com>
2023-05-30 16:46:42 +0200

    Basic bigram

---
+++ NotebookAttention.py
@@ -0,0 +1,51 @@
+import torch # we use PyTorch: https://pytorch.org
+from torch.nn import functional as F
+# consider the following toy example:
+
+torch.manual_seed(1337)
+B,T,C = 4,8,2 # batch, time, channels
+x = torch.randn(B,T,C)
+x.shape
+
+# We want x[b,t] = mean_{i<=t} x[b,i]
+xbow = torch.zeros((B,T,C))
+for b in range(B):
+    for t in range(T):
+        xprev = x[b,:t+1] # (t,C)
+        xbow[b,t] = torch.mean(xprev, 0)
+
+print(x[0])
+print(xbow[0])
+
+# toy example illustrating how matrix multiplication can be used for a "weighted aggregation"
+torch.manual_seed(42)
+a = torch.tril(torch.ones(3, 3))
+a = a / torch.sum(a, 1, keepdim=True)
+b = torch.randint(0,10,(3,2)).float()
+c = a @ b
+print('a=')
+print(a)
+print('--')
+print('b=')
+print(b)
+print('--')
+print('c=')
+print(c)
+
+# Prev nested for are inefficient. Using matrix multiplication to average the sums.
+# This is a better way to implement attention.
+
+# version 2: using matrix multiply for a weighted aggregation
+wei = torch.tril(torch.ones(T, T))
+wei = wei / wei.sum(1, keepdim=True)
+xbow2 = wei @ x # (B, T, T) @ (B, T, C) ----> (B, T, C)
+print(torch.allclose(xbow, xbow2))
+
+
+# version 3: use Softmax
+tril = torch.tril(torch.ones(T, T))
+wei = torch.zeros((T,T))
+wei = wei.masked_fill(tril == 0, float('-inf'))
+wei = F.softmax(wei, dim=-1)
+xbow3 = wei @ x
+torch.allclose(xbow, xbow3)
